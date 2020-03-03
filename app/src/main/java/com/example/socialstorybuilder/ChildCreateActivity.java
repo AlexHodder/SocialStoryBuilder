15ded3192de25ba42d8e526a95190745e0b688d2
AlexHodder <ahw519@york.ac.uk>
2020-03-03 17:08:00 +0000

    activity creation

---
+++ app/src/main/java/com/example/socialstorybuilder/ChildCreateActivity.java
@@ -0,0 +1,14 @@
+package com.example.socialstorybuilder;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChildCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.child_create);
+    }
+}
