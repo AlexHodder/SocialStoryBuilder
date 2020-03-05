3e20fa2d588c103885c421698d6225224c0cfdb9
AlexHodder <ahw519@york.ac.uk>
2020-03-05 16:35:09 +0000

    layout updates

--- app/src/main/java/com/example/socialstorybuilder/ChildLoginActivity.java
+++ app/src/main/java/com/example/socialstorybuilder/ChildLoginActivity.java
@@ -1,8 +1,22 @@
 package com.example.socialstorybuilder;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class ChildLoginActivity extends AppCompatActivity {
 
@@ -10,6 +24,36 @@ public class ChildLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.child_login);
+        SharedPreferences childPreferences = getSharedPreferences("child_users",MODE_PRIVATE);
+        Set<String> fetch = childPreferences.getStringSet("child", null);
+        List<String> childList = new ArrayList<>();
+        if (fetch != null) childList = new ArrayList<>(fetch);
 
+//        childList.add("Jeff");
+//        childList.add("John");
+//        childList.add("Ruth");
+
+        ListView childLayout = findViewById(R.id.child_accounts);
+
+        for (int i = 1; i <= childList.size(); i++){
+            TextView childName = new TextView(this);
+            childName.setText(childList.get(i));
+            childLayout.addView(childName);
+        }
+    }
+
+
+    public void signIn(){
+
+    }
+
+    public void toChildInitial(View view){
+        Intent intent = new Intent(this, ChildInitialActivity.class);
+        startActivity(intent);
+    }
+
+    public void toChildCreate(View view){
+        Intent intent = new Intent(this, ChildCreateActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/socialstorybuilder/MainActivity.java
+++ app/src/main/java/com/example/socialstorybuilder/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.socialstorybuilder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.socialstorybuilder.ui.login.AdultLoginActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +20,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial_layout);
+    }
 
+    public void toChildLogin(View view){
+        Intent intent = new Intent(this, ChildLoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void toAdultLogin(View view){
+        Intent intent = new Intent(this, AdultLoginActivity.class);
+        startActivity(intent);
     }
 
 }
