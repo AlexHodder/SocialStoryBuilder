512a6a7041b6f150f00c5b94ed73b89bbae11c44
Alex <ahw519@york.ac.uk>
2020-03-10 02:10:34 +0000

    child create layout + java updates

--- app/src/main/java/com/example/socialstorybuilder/ChildCreateActivity.java
+++ app/src/main/java/com/example/socialstorybuilder/ChildCreateActivity.java
@@ -1,6 +1,9 @@
 package com.example.socialstorybuilder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,9 @@ public class ChildCreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.child_create);
+    }
+
+    public void selectImage(View view){
+
     }
 }
