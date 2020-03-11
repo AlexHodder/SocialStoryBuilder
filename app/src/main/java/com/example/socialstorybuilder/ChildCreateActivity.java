e41e7cd123cee3aefb00629fe712180922e7d067
Alex <ahw519@york.ac.uk>
2020-03-11 17:40:45 +0000

    child create + manifest updates

--- app/src/main/java/com/example/socialstorybuilder/ChildCreateActivity.java
+++ app/src/main/java/com/example/socialstorybuilder/ChildCreateActivity.java
@@ -1,21 +1,56 @@
 package com.example.socialstorybuilder;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+
 public class ChildCreateActivity extends AppCompatActivity {
+
+    public static final int GALLERY_REQUEST_CODE = 123;
+    private Uri childAvatarImage;
+
+    private ImageView avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.child_create);
+        avatar = findViewById(R.id.imageView);
     }
 
     public void selectImage(View view){
-
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case GALLERY_REQUEST_CODE:
+                    childAvatarImage = data.getData();
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), childAvatarImage);
+                        avatar.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        Log.i("TAG", "Some exception " + e);
+                    }
+                    break;
+        }
+    }
+
 }
+
